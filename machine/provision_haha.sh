1dddfff4b6bc27f47f72ee580b184d568c2f4dab
Saul Johnson <saul.a.johnson@gmail.com>
2019-09-05 12:08:14 +0100

    Fix HAHA installation directory

--- machine/provision_haha.sh
+++ machine/provision_haha.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # Download and unpack HAHA.
+mkdir -p ~/etc
 cd ~/etc
 wget http://haha.mimuw.edu.pl/releases/haha-0.56-2016-12-12-linux-x86_64.zip
 unzip haha-0.56-2016-12-12-linux-x86_64.zip
 rm haha-0.56-2016-12-12-linux-x86_64.zip
 
 # Add to ~/bin (in PATH).
-mkdir ~/bin
+mkdir -p ~/bin
 ln -s ~/etc/haha-0.56/haha ~/bin/haha
 
 # Create desktop shortcut.
